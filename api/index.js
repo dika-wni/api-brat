1a9b7feacbf69fbc4177e385d7ebbf2363c3e4d9
dika-wni <dikaadityafiransyah@gmail.com>
2026-06-18 18:39:34 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 module.exports = async (req, res) => {
     const text = req.query.text || 'Brat';
     
-    // Membuat gambar format SVG secara instan tanpa modul berat
+    // Membuat gambar format SVG secara instan tanpa modul tambahan
     const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512">
         <rect width="512" height="512" fill="#FFFFFF"/>
         <text x="30" y="100" font-family="Arial, Helvetica, sans-serif" font-weight="bold" font-style="italic" font-size="85" fill="#000000">${text.toLowerCase()}</text>
